3ac2187d7e89b5ead420d902c99af65d17202448
gcebreiro <gcebreiro@frba.utn.edu.ar>
2025-12-14 20:32:55 -0300

    Fix update time

--- app/src/main/java/com/example/aquaminder/core/di/RetrofitModule.kt
+++ app/src/main/java/com/example/aquaminder/core/di/RetrofitModule.kt
@@ -45,10 +45,10 @@ object RetrofitModule {
         usernameInterceptor: UsernameInterceptor
     ): OkHttpClient.Builder {
         return OkHttpClient.Builder()
-            .connectTimeout(10, TimeUnit.SECONDS)
-            .writeTimeout(10, TimeUnit.SECONDS)
-            .readTimeout(10, TimeUnit.SECONDS)
-            .callTimeout(10, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .callTimeout(30, TimeUnit.SECONDS)
             .addInterceptor(logging)
             .addInterceptor(usernameInterceptor)
     }
